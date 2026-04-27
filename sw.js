7658114d0f0f46ccaa08230cb7593cbbe9561ac0
cagoooo <cagoooo@users.noreply.github.com>
2026-04-27 01:35:04 +0000

    deploy: f31c95a2f252eaa0436b4ffef451de3e7ab8c754

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  * 版本號變更時，install → activate 會清掉舊 cache。
  */
 
-const SW_VERSION = 'v0.4.2';
+const SW_VERSION = 'v0.4.3';
 const HTML_CACHE = `html-${SW_VERSION}`;
 const STATIC_CACHE = `static-${SW_VERSION}`;
 const RUNTIME_CACHE = `runtime-${SW_VERSION}`;
